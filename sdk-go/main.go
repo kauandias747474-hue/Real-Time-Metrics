e2650767995aba30cd4fb94ce49620902620bbcc
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-21 13:12:55 -0300

    Update main.go

--- sdk-go/main.go
+++ sdk-go/main.go
@@ -1,1 +1,28 @@
+package main
 
+import (
+	"log"
+	"time"
+
+	"sdk-go/client"
+	"sdk-go/metrics"
+)
+
+func main() {
+
+	motor, err := client.InitClient("localhost:50051")
+	if err != nil {
+		log.Fatalf("Falha na conexão: %v", err)
+	}
+
+	defer motor.Conn.Close() 
+
+	log.Println("Conectado ao motor Rust via gRPC Stream.")
+
+	for {
+		m := metrics.NewMetric("SENSOR_A1", 25.4)
+		motor.EnviarParaMotor(m)
+		
+		time.Sleep(2 * time.Second)
+	}
+}
