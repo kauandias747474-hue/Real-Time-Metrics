1c71df1ceb04213826bc3938280d61d64323e5a7
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-21 13:32:36 -0300

    Update udp.rs

--- src/ingestion/udp.rs
+++ src/ingestion/udp.rs
@@ -1,1 +1,33 @@
+use std::net::UdpScoket;
+use crate::storage::sink::DataSink;
 
+pub fn start_udp_listener() {
+
+    let socket = match UdpSocket::bind("127.0.0.1:8080") {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!(" Erro ao abrir porta UDP: {}", e);
+            return;
+        }
+    };
+
+    println!(" [UDP] Servidor escutando na porta 8080...");
+
+    let mut buf = [0; 1024];
+
+    loop {
+     
+        match socket.recv_from(&mut buf) {
+            Ok((size, src)) => {
+                      let raw_data = String::from_utf8_lossy(&buf[..size]);
+                println!("🔌 [UDP] Recebido de {}: {}", src, raw_data);
+
+                
+                DataSink::log_event(&format!("Dado UDP recebido: {}", raw_data));
+            }
+            Err(e) => {
+                eprintln!(" Erro ao receber pacote UDP: {}", e);
+            }
+        }
+    }
+}
