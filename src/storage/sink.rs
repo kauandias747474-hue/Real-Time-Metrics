96eada1326f2acca9c80b23ae9a7562157f508ca
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-22 11:29:37 -0300

    Update sink.rs

--- src/storage/sink.rs
+++ src/storage/sink.rs
@@ -1,1 +1,19 @@
+use std::fs::OpenOptions;
+use std::io::Write;
 
+pub struct DataSink;
+
+impl DataSink {
+
+    pub fn save_metric(sensor_id: &str, value: f64) {
+       let line = format!("ID: {}, Valor: {}\n", sensor_id, value);
+
+ 
+        if let Ok(mut file) = OpenOptions::new().create(true).append(true).open("metrics.db") {
+            let _ = file.write_all(line.as_bytes());
+        }
+
+
+        println!(" [STORAGE] Dado persistido: {}", line.trim());
+    }
+}
