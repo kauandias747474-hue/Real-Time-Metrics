8f5b34d7088bb38ad17c4d8c7543c26b0c3afdee
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-22 11:28:30 -0300

    Update udp.rs

--- src/ingestion/udp.rs
+++ src/ingestion/udp.rs
@@ -1,33 +1,18 @@
-use std::net::UdpScoket;
-use crate::storage::sink::DataSink;
+use std::net::UdpSocket; 
+use crate::storage::DataSink;
 
-pub fn start_udp_listener() {
-
-    let socket = match UdpSocket::bind("127.0.0.1:8080") {
-        Ok(s) => s,
-        Err(e) => {
-            eprintln!(" Erro ao abrir porta UDP: {}", e);
-            return;
-        }
-    };
-
-    println!(" [UDP] Servidor escutando na porta 8080...");
-
+pub fn start_udp_server() {
+ 
+    let socket = UdpSocket::bind("127.0.0.1:8080").expect("Erro ao ligar servidor UDP");
     let mut buf = [0; 1024];
 
-    loop {
-     
-        match socket.recv_from(&mut buf) {
-            Ok((size, src)) => {
-                      let raw_data = String::from_utf8_lossy(&buf[..size]);
-                println!("🔌 [UDP] Recebido de {}: {}", src, raw_data);
+    println!(" Servidor UDP escutando em 127.0.0.1:8080");
 
-                
-                DataSink::log_event(&format!("Dado UDP recebido: {}", raw_data));
-            }
-            Err(e) => {
-                eprintln!(" Erro ao receber pacote UDP: {}", e);
-            }
+    loop {
+        if let Ok((size, _)) = socket.recv_from(&mut buf) {
+            let raw_data = String::from_utf8_lossy(&buf[..size]);
+           
+            DataSink::log_event(&format!("Dado UDP recebido: {}", raw_data));
         }
     }
 }
