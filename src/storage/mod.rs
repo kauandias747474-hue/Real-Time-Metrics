3fc9fbb22c443021e768ffb0e929c7866593a052
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-22 11:29:23 -0300

    Update mod.rs

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,1 +1,19 @@
+pub struct DataSink {
+    
+}
 
+impl DataSink {
+    pub fn new() -> Self {
+        DataSink {}
+    }
+
+  
+    pub fn save_metric(&self, metric: &crate::telemetry::MetricRequest) {
+        println!(" A guardar métrica: {} -> {}", metric.sensor_id, metric.value);
+    }
+
+  
+    pub fn log_event(message: &str) {
+        println!(" LOG: {}", message);
+    }
+}
